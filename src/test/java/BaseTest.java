da9c8d556bc038bfd42cc09c00bb20f1afc01784
K <vinay-kumar.b.k@capgemini.com>
2023-05-01 16:07:44 +0530

    Added listeners

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,22 +1,48 @@
+import com.aventstack.extentreports.MediaEntityBuilder;
 import org.openqa.selenium.WebDriver;
+import org.testng.IResultMap;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import reports.ExtentReport;
 import util.driver.DriverUtils;
+import util.fileUtils.ScreenShotUtil;
 
+import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     WebDriver driver;
+    @BeforeSuite
+    public void beforeSuite(){
+
+//        ExtentReport.initExtentReport(); Extent report codes are moved to listeners
+    }
+
+    @AfterSuite
+    public void afterSuite(){
+//        ExtentReport.prepareReport();
+    }
+
     @BeforeMethod
-    public void initSession() throws MalformedURLException {
+    public void initSession(Method method) throws MalformedURLException {
+//        ExtentReport.createTest(method.getName());
         DriverUtils.initDriver();
         driver = DriverUtils.getDriver();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
 
     @AfterMethod
-    public void tearDown(){
+    public void tearDown(ITestResult result){
+//        if(result.isSuccess()){
+//            ExtentReport.getTest().pass(result.getName() + "has passed");
+//        } else {
+//            String screenShot = ScreenShotUtil.getSscreenShot();
+//            ExtentReport.getTest().fail(result.getName() + " has failed", MediaEntityBuilder.createScreenCaptureFromBase64String(screenShot).build());
+//        }
         DriverUtils.tearDownDriver();
     }
 }
--- src/test/java/POM_Test.java
+++ src/test/java/POM_Test.java
@@ -3,22 +3,26 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.DragAndDropPage;
 import pages.HomePage;
+import reports.ExtentReport;
 
 public class POM_Test extends BaseTest{
 
     @Test
     public void pageFactoryElement(){
         new HomePage().clickOnViews();
+        ExtentReport.getTest().pass("pageFactoryElement passed successfully");
     }
 
     @Test
     public void usingDynamicXpath(){
         new HomePage().clickOnMenu("Views");
+        ExtentReport.getTest().pass("usingDynamicXpath passed successfully");
     }
 
     @Test
     public void usingDynamicPageFactory(){
         new HomePage().clickOnMenuDynamicallyUsingPageFactoryElement("Views");
+        ExtentReport.getTest().pass("usingDynamicPageFactory passed successfully");
     }
 
     @Test
@@ -27,6 +31,7 @@ public class POM_Test extends BaseTest{
         DragAndDropPage dragAndDropPage = new DragAndDropPage();
         dragAndDropPage.performDragDrop();
         Assert.assertTrue(dragAndDropPage.isDragDropSuccessful());
+        ExtentReport.getTest().pass("usingFacadeUtils passed successfully");
     }
 
 }
